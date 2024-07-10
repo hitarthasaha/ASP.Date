959e4b52c0280d45895facc48e7b8ec4daaae56a
Hitartha Saha <2054879@cognizant.com>
2024-07-10 14:44:09 +0530

    Added authentication

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+namespace API.Controllers;
+
+/*public class AccountController : BaseApiController
+{
+    public async Task<ActionResult<AppUser>> Register(string username, string password)
+    {
+        using var hmac = new HMACSHA512();
+
+        
+    }
+}*/
--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using API.Data;
+using API;
+using API.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+// Add services to the container.
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
 var app = builder.Build();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:5075","https://localhost:5075"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
+app.UseAuthentication();
+app.UseAuthorization();
+
 
 app.MapControllers();
 
